6da5f7c316f8900efbfe371435e4f221f1ae955b
Rasmus Jensen <rasje17@student.sdu.dk>
2020-02-25 10:53:45 +0100

    test

---
+++ Wizzard/app/src/main/java/com/example/wizzard/CollectAdressActivity.java
@@ -0,0 +1,14 @@
+package com.example.wizzard;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CollectAdressActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_collect_adress);
+    }
+}
